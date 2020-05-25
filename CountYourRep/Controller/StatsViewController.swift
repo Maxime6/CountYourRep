f358c51d491ce477cb89e9b3a789b5ce689619c4
Maxime6 <maxime.tanter@hotmail.fr>
2020-05-25 22:02:12 +0200

    Set up StatsViewController

---
+++ CountYourRep/Controller/StatsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  StatsViewController.swift
+//  CountYourRep
+//
+//  Created by Maxime on 25/05/2020.
+//  Copyright © 2020 MaximeTanter. All rights reserved.
+//
+
+import UIKit
+
+class StatsViewController: UIViewController {
+
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        segmentedControl.setTitleTextAttributes(titleTextAttributes, for: .selected)
+        segmentedControl.setTitleTextAttributes(titleTextAttributes, for: .normal)
+    }
+
+
+}
